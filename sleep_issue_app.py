ab240a34eacdb83bfcb14e1be5204f4b03c9b5d5
maikelps <maikelpereira2@gmail.com>
2023-08-23 18:36:00 +0100

    1 - streamlit final structure

--- sleep_issue_app.py
+++ sleep_issue_app.py
@@ -5,10 +5,13 @@ from modeling import load_model
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-# Loading model with scaling
-model = load_model('NoSystolic_ScaledModelSVC.pkl')
+# Loading issue model with scaling
+issue_model = load_model('models/NoSystolic_ScaledModelSVC.pkl')
 
-def predict(model, input_data):
+# Loading type of issue model with scaling
+issue_type_model = load_model('models/SleepIssueType_ModelScaled.pkl')
+
+def proba_predict(model, input_data):
     """
     Extracts the probability of having any sleep issue
     """
@@ -20,7 +23,7 @@ def plot_filled_gender(male, percentage):
     fig, ax = plt.subplots(figsize=(1,2))
     fig.patch.set_alpha(0)
 
-    img_path = 'male_silhouette.png' if (male==1) else 'female_silhouette.png'
+    img_path = 'images/male_silhouette.png' if (male==1) else 'images/female_silhouette.png'
 
     # Load image
     img = mpimg.imread(img_path)
@@ -45,6 +48,27 @@ def plot_filled_gender(male, percentage):
 
     # Remove axis
     ax.axis('off')
+    
+    return fig
+
+def sleep_issue_image(issue, TITLE):
+
+    fig, ax = plt.subplots(figsize=(2,2))
+    fig.patch.set_alpha(0)
+
+    img_path = 'images/sleep_apnea.jpeg' if (issue=='Sleep Apnea') else 'images/insomnia.jpeg'
+
+    # Load image
+    img = mpimg.imread(img_path)
+
+    # Display the modified image
+    ax.imshow(img, interpolation='none')
+
+    # Remove axis
+    ax.axis('off')
+
+    # Comment on the issue for the user
+    ax.set_title(TITLE, fontsize=8, color="#FFFFFF")
     
     return fig
 
@@ -87,17 +111,40 @@ if st.button("Predict"):
     print(input_df)
 
     # Predicting ussing the model that already has scaling implemented in the pipeline
-    prediction = predict(model, input_df)*100
+    issue_prob = proba_predict(issue_model, input_df)*100
 
-    print(prediction)
+    print(issue_prob)
 
     st.write(f"### Probability of having a sleep issue:")
 
     # Creating figure
-    fig = plot_filled_gender(is_male, np.round(prediction,2))
+    fig = plot_filled_gender(is_male, np.round(issue_prob,2))
+
+    # This value will control the layout and will give the user extra information 
+    # on the sleep condition it suffers ussing the logistic model.
+    cut_off = 50
 
     # Creating column components for the plot
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns([.2,.8,.1])
     # Show plot in the middle of the app
     with col2:
         st.pyplot(fig, use_container_width=False)
+
+        #sleep_apnea=0 vs insomnia=1
+        issue_prediction = issue_type_model.predict(input_df)[0]
+        print(issue_prediction)
+        print(issue_type_model.predict_proba(input_df))
+
+        issue_prediction = "Sleep Apnea" if (issue_prediction == 1) else "Insomnia"
+
+        if (issue_prob >= cut_off):
+            #st.write(f"It is likely that your sleep issue is {issue_prediction}")
+
+            title = f"You may have {issue_prediction}"
+
+            st.pyplot( sleep_issue_image(issue_prediction, title), use_container_width=False)
+        
+        # Bringing image
+        #fig, ax = plt.subplots(figsize=(1,2))
+        #ax.imshow(grayscale_img_with_alpha, interpolation='none')
+            
